4ca5e10b4bc980e150033b7f1b7cdd2158080f30
Cleber Gasparoto <chgasparoto@gmail.com>
2018-09-30 18:46:59 -0300

    Fixes server url for test suite

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // appUrl: 'https://stark-brushlands-54184.herokuapp.com',
-  serverUrl: 'http://localhost:5000',
+  serverUrl: 'https://stark-brushlands-54184.herokuapp.com',
+  // serverUrl: 'http://localhost:5000',
 };
 
 module.exports = config;
